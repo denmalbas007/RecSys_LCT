98db38a8c635458cc0fe350394ccb3c0d1cc9b22
sashaboriskin <brskn.lx@gmail.com>
2022-10-30 21:20:49 +0300

    second version of baseline

--- ML/baseline.py
+++ ML/baseline.py
@@ -2,19 +2,24 @@ import pandas as pd
 from tqdm import tqdm
 import json
 
+def get_dates_from_period(period: str) -> list:
+    #example of period '09.20-11.21'
+    periods_dates = []
+    start_date = period.split('-')[0]
+    last_date = period.split('-')[1]
+    
 region = 'АЛТАЙСКИЙ КРАЙ'
 period = '09.20-11.21'
-
-'''json struct with needed region and period
-tnved:
-    {
-    import_region:  
-
-
-    }
-
-'''
 
 
 whole_data = pd.read_csv('whole_data.csv')
 unique_tnved = list(whole_data['tnved_6'].unique())
+stoims = dict()
+for i in tqdm(range(len(unique_tnved))):
+    stoims[unique_tnved[i]] = sum(whole_data.loc[whole_data['napr'] == 'ИМ' and 
+    whole_data['napr'] == unique_tnved[i] and 
+    whole_data['period'] in get_dates_from_period(period)]['stoim'])
+
+print(stoims)
+
+
