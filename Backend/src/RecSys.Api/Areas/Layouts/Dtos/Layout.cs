a79ed29d806aa611b701d6d4ed63ea680a275fdf
daniilda <daniilda@hotmail.com>
2022-11-06 10:20:46 +0400

    Грязь.

--- Backend/src/RecSys.Api/Areas/Layouts/Dtos/Layout.cs
+++ Backend/src/RecSys.Api/Areas/Layouts/Dtos/Layout.cs
@@ -1,5 +1,23 @@
+using System.Text.Json.Serialization;
 using RecSys.Api.CommonDtos;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace RecSys.Api.Areas.Layouts.Dtos;
 
-public record Layout(long Id, string Name, Filter Filter, DateTime LastUpdatedAt, DateTime CreatedAt);
+public class Layout
+{
+    public long Id { get; init; }
+
+    public string Name { get; init; } = null!;
+
+    [JsonPropertyName("filter")]
+    public Filter FilterOuter { get; init; } = null!;
+
+    [JsonIgnore]
+    public string Filter
+    {
+        init => FilterOuter = JsonSerializer.Deserialize<Filter>(value)!;
+    }
+
+    public DateTime UpdatedAt { get; init; }
+}
--- Backend/src/RecSys.Api/Areas/Reports/Dtos/ReportMetadata.cs
+++ Backend/src/RecSys.Api/Areas/Reports/Dtos/ReportMetadata.cs
@@ -7,4 +7,10 @@ public class ReportMetadata
     public string Name { get; init; } = null!;
 
     public DateTime CreatedAt { get; init; }
+
+    public string? PdfUrl { get; init; }
+
+    public string? ExcelUrl { get; init; }
+
+    public bool IsReady { get; init; }
 }
